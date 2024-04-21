d0f94f319cc6ee6ce6ab8a85870a03a5cbdd063a
Amanuel bekele <amanuelbekele771@gmail.com>
2024-04-21 13:32:40 -0700

    add new feature

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const expressLayouts = require('express-ejs-layouts')
-const router = require('./public/index/routes/index')
+const router = require('./public/views/routes/index')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
